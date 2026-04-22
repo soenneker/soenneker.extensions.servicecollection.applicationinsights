44f8c4dfe6c5a132f1d78d92657bbd774e4dc375
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:02 -0500

    TUnit conversion

--- test/Soenneker.Extensions.ServiceCollection.ApplicationInsights.Tests/ServiceCollectionApplicationInsightsExtensionTests.cs
+++ test/Soenneker.Extensions.ServiceCollection.ApplicationInsights.Tests/ServiceCollectionApplicationInsightsExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.ServiceCollection.ApplicationInsights.Tests;
 
 public sealed class ServiceCollectionApplicationInsightsExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
